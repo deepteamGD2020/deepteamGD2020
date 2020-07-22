98c98e9e86109991166db700014922d8253d8ac5
Berglind Brá Jóhannsdóttir <berglindbrajohannsdottir@Berglinds-MacBook-Pro.local>
2020-07-22 21:38:53 +0200

    scroll to js

--- script-launch.js
+++ script-launch.js
@@ -69,6 +69,11 @@
             $(".hideOverview").hide();
             $(".usAll").addClass('hide');
         });
+        $(".usAll").click(function(){
+            $(".showOverview").show();
+            $(".hideOverview").hide();
+            $(".usAll").addClass('hide');
+        });
 
 
 
@@ -508,3 +513,38 @@
         window.addEventListener("load", lazyload);
       }
     });
+
+
+
+
+
+
+
+// SCROLL OVERVIEW TO ======================
+// ================================
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
