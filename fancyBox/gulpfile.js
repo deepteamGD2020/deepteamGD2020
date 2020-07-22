8840225f7aec3350ca57b1689d9d0d141cf6a097
tranghasgit <tranghasgit@users.noreply.github.com>
2020-07-22 09:59:46 +0200

    fancybox

--- fancyBox/gulpfile.js
+++ fancyBox/gulpfile.js
@@ -1,76 +1,68 @@
-/* jshint node: true */
-/* global $: true */
-"use strict";
-
 var gulp = require("gulp"),
-    $ = require("gulp-load-plugins")();
-var config = {
-        js: [
-            "lib/jquery.mousewheel.pack.js",
-            "source/jquery.fancybox.pack.js",
-            "source/helpers/jquery.fancybox-buttons.js",
-            "source/helpers/jquery.fancybox-thumbs.js",
-            "source/helpers/jquery.fancybox-media.js"
-        ],
-        css: [
-            "source/jquery.fancybox.css",
-            "source/helpers/jquery.fancybox-buttons.css",
-            "source/helpers/jquery.fancybox-thumbs.css"
-        ],
-        images: [
-            "source/helpers/**/*.{jpg,png,svg,gif,webp,ico}",
-            "source/*.{jpg,png,svg,gif,webp,ico}"
-        ]
-    },
-    dist = {
-        images: "dist/images/fancybox",
-        css: "dist/css",
-        js: "dist/js"
-    };
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  cssnano = require("gulp-cssnano"),
+  autoprefixer = require("gulp-autoprefixer"),
+  header = require("gulp-header"),
+  replace = require("gulp-replace");
 
+var pkg = require("./package.json");
+var banner = [
+  "// ==================================================",
+  "// fancyBox v${pkg.version}",
+  "//",
+  "// Licensed GPLv3 for open source use",
+  "// or fancyBox Commercial License for commercial use",
+  "//",
+  "// http://fancyapps.com/fancybox/",
+  "// Copyright ${new Date().getFullYear()} fancyApps",
+  "//",
+  "// ==================================================",
+  ""
+].join("\n");
 
-/*
- - |--------------------------------------------------------------------------
- - | Gulp Front Tasks
- - |--------------------------------------------------------------------------
- - |
- + *
- + *
- */
+// Concatenate & Minify JS
 
-gulp.task("clean", function () {
-    return gulp.src("dist", {read: false})
-        .pipe($.clean());
+gulp.task("scripts", function() {
+  return gulp
+    .src([
+      "src/js/core.js",
+      "src/js/media.js",
+      "src/js/guestures.js",
+      "src/js/slideshow.js",
+      "src/js/fullscreen.js",
+      "src/js/thumbs.js",
+      "src/js/hash.js",
+      "src/js/wheel.js"
+    ])
+    .pipe(concat("jquery.fancybox.js"))
+    .pipe(replace(/({fancybox-version})/g, pkg.version))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest("dist"))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(uglify())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task("scripts", function () {
-    return gulp.src(config.js)
-        .pipe($.plumberNotifier())
-        .pipe($.concat("jquery.fancybox.min.js"))
-        .pipe($.uglify())
-        .pipe(gulp.dest(dist.js));
+// Compile CSS
+
+gulp.task("css", function() {
+  return gulp
+    .src("src/css/*.css") // Gets all files src/css
+    .pipe(
+      autoprefixer({
+        browsers: ["last 5 versions"],
+        cascade: false
+      })
+    )
+    .pipe(concat("jquery.fancybox.css"))
+    .pipe(gulp.dest("dist"))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(cssnano({zindex: false}))
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task("styles", function () {
-    return gulp.src(config.css)
-        .pipe($.plumberNotifier())
-        .pipe($.concat("jquery.fancybox.min.css"))
-        .pipe($.autoprefixer("last 5 version"))
-        .pipe($.replace(/url\('?(.*)'?\)/g, "url('../images/fancybox/$1')"))
-        .pipe($.replace("''", "'"))
-        .pipe($.cleanCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest(dist.css))
-});
-
-gulp.task("images", function () {
-    return gulp.src(config.images)
-        .pipe($.newer(dist.images))
-        .pipe(gulp.dest(dist.images));
-});
-
-
-gulp.task('default', ["images", "styles", "scripts"]);
-
-
-
- 
+// Default Task
+gulp.task("default", ["scripts", "css"]);
